24503459755832daae5f4ad4a252eefd9b0e9ffa
tnusser <tobiasnusser@web.de>
2022-01-02 16:53:54 +0100

    Duplicate elimination methods

--- mongo_db.py
+++ mongo_db.py
@@ -1,14 +1,11 @@
-import os
-
 import pymongo.errors
 from pymongo import MongoClient
-import json
-import time
 from helper import logger
+from bson.objectid import ObjectId
 
 
 def create_collection(collection):
-    if collection not in db.collection_names():
+    if collection not in db.list_collection_names():
         db.create_collection(collection)
 
 
@@ -49,6 +46,50 @@ def create_indexes(collection, index_name="id"):
             collection.create_index(index_name, unique=True)
         except pymongo.errors.OperationFailure:
             logger.warning("Index already exists, creation not possible")
+
+
+def del_duplicate_follows(collection_name, attribute):
+    collection = db[collection_name]
+    response = collection.aggregate(
+        [{'$group': {'_id': '$' + attribute, 'count': {'$sum': 1}}}, {'$match': {'count': {'$gt': 1}}}])
+    for el in response:
+        res = read({"id": el["_id"]}, "cc_users", {"id": 1, "followers_crawled": 1})
+        for re in res:
+            if not re["followers_crawled"]:
+                result = collection.delete_one({'_id': ObjectId(re["_id"])})
+                print(result.raw_result)
+    response = read({}, "cc_follows", {"id": 1, "following": 1, "followed_by": 1})
+    for res in response:
+        if "following" in res:
+            print(res["following"])
+        if "followed_by" in res:
+            print(res["followed_by"])
+        break
+
+
+def update_dup(id, arr, dups):
+    collection = db["cc_follows"]
+    collection.update_one({"_id": id}, {"$set": {arr: dups}})
+
+
+def del_dup_array(collection_name, attribute):
+    c = 0
+    response = read({}, "cc_follows", {"id": 1, "following": 1, "followed_by": 1})
+    for res in response:
+        if c % 1000 == 0:
+            print(c)
+        c += 1
+        if "following" in res:
+            original = res["following"]
+            dup_free = list(set(original))
+            if not len(dup_free) == len(original):
+                update_dup(res["_id"], "following", dup_free)
+                # print(res["_id"])
+        if "followed_by" in res:
+            original = res["followed_by"]
+            dup_free = list(set(original))
+            if not len(dup_free) == len(original):
+                update_dup(res["_id"], "followed_by", dup_free)
 
 
 client = MongoClient('mongodb://127.0.0.1:27017/')
