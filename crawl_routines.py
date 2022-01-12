0f45e4d8305e5f9c497fbd7f1cc0115eef325a7f
tnusser <tobiasnusser@web.de>
2022-01-12 14:51:14 +0100

    Added some cc events

--- crawl_routines.py
+++ crawl_routines.py
@@ -543,9 +543,22 @@ class EventSearch:
 event_list = [
     EventSearch(-1, "1442243266280370177", start_date=None, hashtags=None, username=None, comment="vanderhorst"),
     EventSearch(0, "1433361036191612930", start_date=None, hashtags=None, username=None, comment="toni test"),
-    EventSearch(1, tweet_id="1158074774297468928", start_date="2019-08-03T23:59:59.000Z",
-                hashtags=["neildegrassetyson"],
+    EventSearch(1, "1158074774297468928", start_date="2019-08-04T18:00:00.000Z", hashtags=["#neildegrassetyson"],
                 username="neiltyson", comment="neil de grasse tyson"),
+    EventSearch(2, "1265998625836019712", start_date="2020-06-28T14:00:00.000Z", hashtags=None,
+                username="davidshor", comment="david shor"),
+    EventSearch(3, "1269382518362509313", start_date="2020-06-06T23:00:00.000Z", hashtags=None,
+                username="jk_rowling", comment="j.k. rowling 1"),
+    EventSearch(4, "1269389298664701952", start_date="2020-06-07T12:00:00.000Z", hashtags=None,
+                username="jk_rowling", comment="j.k. rowling 2"),
+    EventSearch(5, None, start_date="2018-05-17T23:00:00.000Z", hashtags=["#AaronMSchlossberg", "#AaronSchlossberg"],
+                username=None, comment="aaron schlossberg"),
+    EventSearch(6, None, start_date="2018-05-08T23:00:00.000Z", hashtags=["#KellyPocha"],
+                username=None, comment="kelly pocha"),
+    EventSearch(7, "1324385598539399168", start_date="2020-11-05T14:00:00.000Z", hashtags=["#FireGinaCarano"],
+                username="ginacarano", comment="gina carano"),
+    EventSearch(8, "1327806477923323904", start_date="2020-11-15T14:00:00.000Z", hashtags=["#FireGinaCarano"],
+                username="ginacarano", comment="gina carano")
 ]
 
 # out_file = open("output/crawl_tweets.txt", "w")
